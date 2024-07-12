cdc950edf17cc072055bf4086cb35ce34667d47a
anjeh24 <junez24@yahoo.com>
2024-07-12 19:59:23 -0400

    iterating through adventurers inventory array elements

--- adventurer.js
+++ adventurer.js
@@ -1,3 +1,7 @@
+
+
+
+
 const adventurer = {
     name: "Robin",
     health: 10,
@@ -8,7 +12,12 @@ const adventurer = {
         companion: {
             name: "Frank",
             type: "Flea",
-            inventory: [hat, sunglasses]
+            inventory: ["hat", "sunglasses"]
         }
     }
     }
+
+    
+for ( i = 0; i <= adventurer.inventory.length; i++) {
+   console.log(adventurer.inventory[i]);
+}
