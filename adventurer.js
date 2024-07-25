43fb9da189443ed385136b8c2e9e2ae12b2eab3e
anjeh24 <junez24@yahoo.com>
2024-07-24 20:18:17 -0400

    tinkering with oop

--- adventurer.js
+++ adventurer.js
@@ -1,7 +1,4 @@
 
-
-
-
 const adventurer = {
     name: "Robin",
     health: 10,
@@ -21,3 +18,18 @@ const adventurer = {
 for ( i = 0; i <= adventurer.inventory.length; i++) {
    console.log(adventurer.inventory[i]);
 }
+
+//Robins method
+
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: ...
+    roll (mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`)
+         }
+    }
+
+   adventurer.roll()
